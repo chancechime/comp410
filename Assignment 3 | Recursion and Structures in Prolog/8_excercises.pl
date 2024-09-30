2de1bc2cde7aecac8e229de9d98075c7df05cb5c
Chance Chime <cchime82@icloud.com>
2024-09-30 15:21:36 -0700

    Finished Assignment 3

---
+++ Assignment 3 | Recursion and Structures in Prolog/8_excercises.pl
@@ -0,0 +1,62 @@
+% Write a procedure that calculates the sum of the numbers in
+% a given range.  The parameters to this procedure should be
+% as follows:
+%
+% 1. The low end of the range
+% 2. The high end of the range
+% 3. The sum of the values in the range
+%
+% If the low end equals the high end, then the sum should be
+% whatever value is equal to the low/high end (the range is
+% inclusive).  If the low end is greater than the high end,
+% then failure should occur.
+
+sumAll(A, B, C) 
+    :- A < B,
+    C is A + B.
+
+% With your summation procedure in hand, the following queries
+% should all succeed (do NOT simply hardcode these values!):
+
+% QUERY 1: sumAll(5, 5, 5).
+% QUERY 2: sumAll(5, 6, 11).
+% QUERY 3: sumAll(0, 3, 6).
+
+% The directPrereq procedure below documents some of the
+% prerequisite courses in the course catalog.  Specifically,
+% it documents which courses are direct prerequisites to others.
+% It should be read as "the first course has the second course as
+% a direct prerequisite"
+
+directPrereq(comp410, comp333).
+directPrereq(comp410, comp282).
+directPrereq(comp410, comp310).
+directPrereq(comp333, comp282).
+directPrereq(comp282, comp182).
+directPrereq(comp282, math150A).
+directPrereq(comp182, comp110).
+directPrereq(comp110, math150A).
+directPrereq(comp310, comp256).
+directPrereq(comp256, comp182).
+directPrereq(comp256, math150A).
+directPrereq(comp256, phil230).
+
+% Write a procedure named prereq which will determine
+% if a course transitively has another course as a prerequisite.
+% For example, COMP 110 is a prerequisite to COMP 410, even though
+% COMP 110 is not a direct prerequisite to COMP 410.  As a hint,
+% course A can be a prerequisite to course B in two distinct ways:
+%
+% 1.) Course A is a direct prerequisite to course B (determined
+%     with the help of directPrereq).  This serves as a base case.
+% 2.) Course A is a direct prerequisite to course C, and course C
+%     is a prerequisite (but not necessarily a direct prerequisite)
+%     to course B.  This serves as a recursive case.
+
+prereq(A, B)
+    :- directPrereq(A, B).
+prereq(A, B)
+    :- directPrereq(A, C),
+    prereq(C, B).
+
+% Finished Assignment 3
--- testingGrounds/intro.pl
+++ testingGrounds/intro.pl
@@ -11,4 +11,16 @@ likes(bob, salad).
 likes(bob, milks).
 
 % Bill likes food served warm
-likes(bill, bacon, warm)
+likes(bill, bacon, warm).
+
+% def myAppend(list1, list2):
+%     if isinstance(list1, Nil):
+%         return list2
+%     elif: isinstance(list1, Cons):
+%         rest = myAppend(list1.tail, list2)
+%         return Cons(list1.head, rest)
+
+myAppend(nil, List, List).
+myAppend(cons(Head, Tail), List2, Output) :-
+    Output = cons(Head, Rest),
+    myAppend(Tail, List2, Rest).
